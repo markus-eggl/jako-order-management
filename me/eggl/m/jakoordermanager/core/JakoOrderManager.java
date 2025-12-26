25bb5dfcbd0966c35cbaa7cb754f9a1a3902cd38
markus-eggl <markus.eggl@posteo.de>
2025-12-26 12:03:27 +0100

    Move main to module ui

--- me/eggl/m/jakoordermanager/core/JakoOrderManager.java
+++
@@ -1,9 +0,0 @@
-package me.eggl.m.jakoordermanager.core;
-
-
-public class JakoOrderManager{
-	public static void main(String[] args) {
-		System.out.println("Test");
-	}
-}
-
---
+++ me/eggl/m/jakoordermanager/ui/OrderManagerUi.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package me.eggl.m.jakoordermanager.ui;
+
+/**
+ * 
+ */
+public class OrderManagerUi {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test");
+
+	}
+
+}
